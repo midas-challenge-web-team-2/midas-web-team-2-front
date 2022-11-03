1d7ccf77168e7794827196dad662452d17c69eff
강석현 <tjrgus0713@gmail.com>
2022-11-03 15:30:12 +0900

    feat: 인풋 컴퍼넌트 추가

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { ChangeEvent, useState } from 'react';
+
+interface Props extends InputStyleProps {
+  type?: 'password' | 'text';
+  placeholder?: string;
+  name?: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+interface InputStyleProps {
+  width?: number;
+  height?: number;
+  disabled?: boolean;
+}
+
+const Input = ({
+  width = 380,
+  height = 56,
+  placeholder,
+  name,
+  value,
+  onChange,
+  disabled,
+  type = 'text',
+}: Props) => {
+  const [isInVisible, setIsInVisible] = useState(type === 'password');
+  return (
+    <_Wrapper width={width} height={height}>
+      <input
+        value={value}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        type={isInVisible ? 'password' : 'text'}
+      />
+      {type === 'password' && <img />}
+    </_Wrapper>
+  );
+};
+
+export default Input;
+
+const _Wrapper = styled.label<InputStyleProps>`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.color.gray4};
+  border-radius: 12px;
+
+  > input {
+    width: calc(${({ width }) => width || 380 - 60}px);
+    height: 100%;
+    color: ${({ theme }) => theme.color.gray9};
+
+    ::placeholder {
+      color: ${({ theme }) => theme.color.gray5};
+      font-size: 14px;
+    }
+  }
+  > img {
+    margin-left: auto;
+  }
+`;
