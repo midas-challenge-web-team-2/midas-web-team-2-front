d1b83c44c799da72f649fb08bf04959d22377e03
강석현 <tjrgus0713@gmail.com>
2022-11-04 02:45:05 +0900

    feat: 최초 로그인 페이지 추가 및 페이지 이동 로직 추가

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import { Route } from 'react-router';
 import MainPage from './pages/Main';
 import AuthCodePage from './pages/AuthCode';
 import ManageWorkType from './pages/ManageWorkType';
+import FirstLogin from './pages/FirstLogin';
 
 const Router = () => {
   return (
@@ -15,6 +16,7 @@ const Router = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/authorization" element={<AuthCodePage />} />
         <Route path="/manage-working-type" element={<ManageWorkType />} />
+        <Route path="/first-login" element={<FirstLogin />} />
       </Routes>
     </BrowserRouter>
   );
